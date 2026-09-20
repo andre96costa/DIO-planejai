dd99970b304fbf77c84eba7de53be371f915ea06
Andre Costa <andre2396costa@gmail.com>
2026-09-20 13:44:06 -0300

    Form with actions

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,20 +1,43 @@
 import { PiggyBank } from "lucide-react"
 import { FormStep } from "./FormStep"
 import { StepProgress } from "./Progress"
+import { simulationFormSteps } from "../../../data/simulation";
+import { useState } from "react";
 
 export const SimulationForm = () => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  // const [formData, setFormData] = useState<SimulationFormData>(
+  //   {} as SimulationFormData,
+  // )
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
+
+  const handleNextStep = (value: string) => {
+    if (currentStepIndex + 1 > totalSteps - 1) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev + 1)
+  }
+
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) {
+      return
+    }
+
+    setCurrentStepIndex((prev) => prev - 1)
+  }
+
   return (
     <>
-        <StepProgress currentStep={1} totalSteps={6} />
+        <StepProgress currentStep={currentStepIndex + 1} totalSteps={totalSteps} />
         <FormStep 
-          icon={PiggyBank} 
-          title="Renda Mensal bruta" 
-          question="Quanto é depositado na sua conta todo mês (somando todas as fontes)"
-          inputProps={{
-            type: 'text',
-            placeholder: 'ex: 50000',
-            prefix: 'R$'
-          }}
+          key={currentStep.id}
+          {...currentStep}
+          onBack={handlePreviousStep}
+          onNext={handleNextStep}
+          hideBackButton={currentStepIndex === 0}
         />
     </>
   )
--- src/components/features/Simulation/FormStep.tsx
+++ src/components/features/Simulation/FormStep.tsx
@@ -1,8 +1,10 @@
 import { ArrowLeft, ArrowRight, type LucideIcon } from "lucide-react"
 import { Input, type InputProps } from "../../shared/Input";
 import { Button } from "../../shared/Button";
+import { useState, type SyntheticEvent } from "react";
 
-interface FormStepProps {
+export interface FormStepProps {
+    id: string;
     icon: LucideIcon;
     title: string;
     question: string;
@@ -13,9 +15,23 @@ interface FormStepProps {
     }
 }
 
+interface ActionsButtonsProps {
+  onBack: () => void
+  onNext: (value: string) => void
+  hideBackButton?: boolean
+}
 
-export const FormStep = ({ icon: Icon, title, question, inputProps, submitButtonProps }: FormStepProps) => {
-  return (
+
+export const FormStep = ({ icon: Icon, title, question, inputProps, submitButtonProps, hideBackButton, onBack, onNext }: FormStepProps & ActionsButtonsProps) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!inputValue) return
+        onNext(inputValue)
+    }
+  
+    return (
     <div className="bg-card rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] sm:p-8">
         <div className="bg-primary mb-4 flex h-15 w-15 items-center justify-center rounded-xl">
             <Icon size={32} className="text-primary-foreground" />
@@ -26,24 +42,26 @@ export const FormStep = ({ icon: Icon, title, question, inputProps, submitButton
         <h3 className="text-foreground mb-6 text-xl leading-snug font-semibold sm:text-2xl">
             {question}
         </h3>
-        <form className="flex flex-col gap-4">
-            <Input {...inputProps} />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <Input {...inputProps} value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
             <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
+                {!hideBackButton && (
                 <Button 
                     type="button"
                     variant='ghost'
                     className="order-2 flex-1 justify-center rounded-xl py-3 sm:order-1"
                     icon={ArrowLeft}
-                    // onClick={onBack}
+                    onClick={onBack}
                 >
                     Voltar
                 </Button>
+                )}
                 <Button 
                     type="submit"
                     variant='primary'
                     className="order-1 flex-1 sm:order-1"
                     icon={!submitButtonProps ? ArrowRight : undefined}
-                    // onClick={onBack}
+                    disabled={!inputValue}
                 >
                     {submitButtonProps?.label ?? 'Próximo'}
                     {submitButtonProps?.emojiIcon}
