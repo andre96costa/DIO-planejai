db48333d2d35283a6bda8296064d73a79e9f04b9
Andre Costa <andre2396costa@gmail.com>
2026-09-20 16:45:24 -0300

    salvando no localstorage as respostas

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,20 +1,27 @@
 import { PiggyBank } from "lucide-react"
 import { FormStep } from "./FormStep"
 import { StepProgress } from "./Progress"
-import { simulationFormSteps } from "../../../data/simulation";
+import { simulationFormSteps, type SimulationFormData } from "../../../data/simulation";
 import { useState } from "react";
+import { useSimulationStorage } from "../../../hooks/useSimulationStorage";
+import { useNavigate } from "react-router-dom";
 
 export const SimulationForm = () => {
+  const { saveFormData } = useSimulationStorage();
+  const navigate = useNavigate();
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const [formData, setFormData] = useState<SimulationFormData>({} as SimulationFormData)
 
-  // const [formData, setFormData] = useState<SimulationFormData>(
-  //   {} as SimulationFormData,
-  // )
   const totalSteps = simulationFormSteps.length
   const currentStep = simulationFormSteps[currentStepIndex]
 
   const handleNextStep = (value: string) => {
+    const updatedFormData = { ...formData, [currentStep.id]: value}
+    setFormData(updatedFormData)
+
     if (currentStepIndex + 1 > totalSteps - 1) {
+      saveFormData(updatedFormData);
+      void navigate('/');
       return
     }
 
